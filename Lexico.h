e2810f8c2529a86ca725d8dbe7b23b1527c8f544
2023JoshuaP <jphilco@unsa.edu.pe>
2024-05-28 20:55:50 -0500

    Implementando clase Lexico, basado en implementacion de Laboratorio en clase.

---
+++ Lexico.h
@@ -0,0 +1,39 @@
+#ifndef LEXICO_H
+#define LEXICO_H
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <map>
+#include <string>
+
+#define MAYOR '>'
+#define MENOR '<'
+#define IGUAL '='
+#define NOT '!'
+#define PUNTOYCOMA ';'
+#define COMA ','
+#define PARI '('
+#define PARD ')'
+#define CORI '['
+#define CORD ']'
+#define LLAVEI '{'
+#define LLAVED '}'
+#define ID 256
+#define NUM 257
+#define ASIGNACION 258
+using namespace std;
+
+class Lexico {
+    public:
+        Lexico(int argc, char* argv[]);
+        ~Lexico();
+        void run();
+    private:
+        int scanner();
+        void mostrar(int token);
+        int isReserved();
+};
+
+#endif
