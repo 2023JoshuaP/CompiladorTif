20c8bc2d786775b26f7b3c1cbac38a3670d8a705
2023JoshuaP <jphilco@unsa.edu.pe>
2024-07-24 11:40:28 -0500

    Redefinicion del Analizador Lexico.

--- Compilador/AnalizadorLexico.h
+++ Compilador/AnalizadorLexico.h
@@ -1,15 +1,13 @@
 #ifndef ANALIZADORLEXICO_H
 #define ANALIZADORLEXICO_H
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <cctype>
 using namespace std;
-
 enum class TokenType {
-    Keyword, Identifier, Number, String, Operator, Separator, Comment, EndOfFile, Error
+    Keyword, Identifier, Integer, Real, Boolean, Character, String, Operator, Separator, Comment, EndOfFile, Error
 };
 
 struct Token {
@@ -20,29 +18,27 @@ struct Token {
 };
 
 class AnalizadorLexico {
-    public:
-        AnalizadorLexico(const string& source);
+public:
+    AnalizadorLexico(const string& source);
+    vector<Token> tokenize();
+private:
+    string source;
+    size_t position;
+    int line;
+    int column;
 
-        vector<Token> tokenize();
-
-    private:
-        string source;
-        size_t position;
-        int line;
-        int column;
-
-        void advance();
-
-        void tokenizeComment(vector<Token>& tokens);
-        void tokenizeIdentifierOrKeyword(vector<Token>& tokens);
-        void tokenizeNumber(vector<Token>& tokens);
-        void tokenizeString(vector<Token>& tokens);
-        void tokenizeOperator(vector<Token>& tokens);
-        void tokenizeSeparator(vector<Token>& tokens);
-
-        bool isKeyword(const string& value);
-        bool isOperator(char ch);
-        bool isSeparator(char ch);
+    void advance();
+    void tokenizeComment();
+    void tokenizeIdentifierOrKeyword(vector<Token>& tokens);
+    void tokenizeNumber(vector<Token>& tokens);
+    void tokenizeString(vector<Token>& tokens);
+    void tokenizeOperator(vector<Token>& tokens);
+    void tokenizeSeparator(vector<Token>& tokens);
+    void tokenizeCharacter(vector<Token>& tokens);
+    bool isKeyword(const string& value);
+    bool isBoolean(const string& value);
+    bool isOperator(char ch);
+    bool isSeparator(char ch);
 };
 
 #endif
