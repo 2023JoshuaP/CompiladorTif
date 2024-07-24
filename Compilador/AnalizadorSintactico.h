68f977b95d463efd38e1667253f7250e26146045
2023JoshuaP <jphilco@unsa.edu.pe>
2024-07-24 11:41:23 -0500

    Redefiniendo Analizador Sintactico.

--- Compilador/AnalizadorSintactico.h
+++ Compilador/AnalizadorSintactico.h
@@ -1,65 +1,47 @@
 #ifndef ANALIZADORSINTACTICO_H
 #define ANALIZADORSINTACTICO_H
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stack>
-#include "./AnalizadorLexico.h"
-using namespace std;
-
-struct ParserState {
-    size_t position;
-    Token currentToken;
-};
+#include "ast.h"
+#include "AnalizadorLexico.h"
 
 class AnalizadorSintactico {
-    public:
-        AnalizadorSintactico(const vector<Token>& tokens);
-        void parse();
-    private:
-        vector<Token> tokens;
-        size_t position;
-        stack<ParserState> stateStack;
+public:
+    AnalizadorSintactico(vector<Token>& tokens_): tokens(tokens_), tokenIndex(0) {
+        if (!tokens.empty()) {
+            currentToken = tokens[tokenIndex];
+        } else {
+            cout<<"Tokens not found"<<endl;
+        }
+    }
+    ASTNodePtr parse();
 
-        Token currentToken();
-        void advance();
+private:
+    vector<Token> tokens;
+    size_t tokenIndex;
+    Token currentToken;
 
-        void match(TokenType type);
-        void error(const string& message);
-
-        void pushState();
-        void popState();
-        void restoreState();
-
-        void parsePseudoCode();
-        void parseNombreAlgoritmo();
-        void parseCuerpo();
-        void parseDeclaracion();
-        void parseDeclaraVariable();
-        void parseTipo();
-        void parseAsignacion();
-        void parseOperadorAsignacion();
-        void parseLectura();
-        void parseImpresion();
-        void parseDecision();
-        void parseDecisionSimple();
-        void parseDecisionMultiple();
-        void parseRepeticion();
-        void parseCicloPara();
-        void parseInicializacionPara();
-        void parseAsignacionPara();
-        void parseOperadorAutoincremento();
-        void parseCicloMientras();
-        void parseCicloHagaMientras();
-        void parseExpresion();
-        void parseExpresionEntera();
-        void parseExpresionReal();
-        void parseExpresionCadena();
-        void parseExpresionBooleana();
-        void parseExpresionCaracter();
-        void parseVariable();
-        void parseIdentificador();
+    void advance();
+    Token peek(size_t offset = 0);
+    void expect(TokenType type);
+    void expect(TokenType type, std::string asd);
+    
+    ASTNodePtr parseProgram();
+    ASTNodePtr parseBlock();
+    ASTNodePtr parseStatement();
+    ASTNodePtr parseDeclaration();
+    ASTNodePtr parseAssignment();
+    ASTNodePtr parseRead();
+    ASTNodePtr parsePrint();
+    ASTNodePtr parseIf();
+    ASTNodePtr parseWhile();
+    ASTNodePtr parseDoWhile();
+    ASTNodePtr parseFor();
+    ASTNodePtr parseSwitch();
+    ASTNodePtr parseExpression();
+    ASTNodePtr parseExpression(TokenType type);
+    ASTNodePtr parseExpressionBooleana();
+    ASTNodePtr parseExpressionForInit();
+    ASTNodePtr parseExpressionForAsign();
 };
 
 #endif
